556a18902655f7e13cc000d629f6fc1c9a59c4fa
kennethclng <78772626+kennethclng@users.noreply.github.com>
2025-11-07 10:08:12 -0800

    Create main.tf

---
+++ modules/web-server/main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "lab10" {
+	ami = var.ami
+	instance_type = var.instance_type
+	key_name = var.key_name
+	subnet_id = var.subnet_id
+	vpc_security_group_ids = var.vpc_security_group_ids
+
+	tags = {
+		Name = "${var.project_name}"
+		Project = var.project_name
+	}
+}
